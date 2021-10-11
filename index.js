2579e9c66e35f69408a47afe2241335baa9e954b
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-11 18:22:35 -0400

    Task 4 test passing

--- index.js
+++ index.js
@@ -110,9 +110,13 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCb) {
+  return {
+    Home: inningCb(),
+    Away: inningCb()
+  }
 }
+console.log('Task 4', getInningScore(inning));
 
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
@@ -156,8 +160,8 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard() {
+  
 }
 
 
